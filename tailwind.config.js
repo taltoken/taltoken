a95542dfafd51c299bdb1da2ab7776749e04b77b
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-07-20 10:01:24 +0300

    chore(format): prettify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layout/**/*.{js,ts,jsx,tsx}'
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['Roboto', 'system-ui'],
-      }
+        poppins: ["Roboto", "system-ui"],
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/ui')],
-}
+  plugins: [require("@tailwindcss/ui")],
+};
