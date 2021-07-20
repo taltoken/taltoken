48abbb237c33595ca1259043364933bb469fe79c
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-07-20 11:56:40 +0300

    Updates

--- _next/static/wHA9jaLhSb1mHn3fKp_jW/_buildManifest.js
+++
@@ -1,8 +0,0 @@
-(self.__BUILD_MANIFEST = {
-  __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
-  "/": ["static/chunks/pages/index-719eed12868ef5126de3.js"],
-  "/_error": ["static/chunks/pages/_error-0b3678de1a16493f25ef.js"],
-  "/support": ["static/chunks/pages/support-e9553682afa6bc041907.js"],
-  sortedPages: ["/", "/_app", "/_error", "/support"],
-}),
-  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
