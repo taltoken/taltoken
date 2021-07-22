fe9766d8496b479cfb1d6661db8fb7994ee7b997
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-07-22 14:29:47 +0300

    feat(social): correct links for discord and github

--- common/constants.ts
+++ common/constants.ts
@@ -1,8 +1,8 @@
 // internal and external links (urls)
-export const GITHUB_URL = "https://github.com/taltoken";
+export const GITHUB_URL = "https://github.com/taraldefi";
 export const STACKS_URL = "https://www.stacks.co/";
 export const DOCS_URL = "";
-export const DISCORD_URL = "https://discord.gg/";
+export const DISCORD_URL = "https://discord.gg/7cuVbGuS";
 export const TWITTER_URL = "https://twitter.com/defitaral";
 export const TELEGRAM_URL = "";
 export const EMAIL_ADDRESS = "taraldefi@gmail.com";
