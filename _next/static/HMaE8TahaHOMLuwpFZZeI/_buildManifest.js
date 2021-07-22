c5751fd871987df5d3f3ffefb93a14b5f0931e9f
dorucioclea <39082034+dorucioclea@users.noreply.github.com>
2021-07-22 11:30:37 +0000

    Deploying to gh-pages from @ taraldefi/taraldefi.github.io@fe9766d8496b479cfb1d6661db8fb7994ee7b997 🚀

---
+++ _next/static/HMaE8TahaHOMLuwpFZZeI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6977cb120189081619df.js"],"/_error":["static/chunks/pages/_error-0c39d77656e35230a6b6.js"],"/support":["static/chunks/pages/support-2bc8c87bc5418d4afa1d.js"],sortedPages:["/","/_app","/_error","/support"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
